3b132edde6d5f5598595bfddeb63d6ce25aca2cb
Your Name <you@example.com>
2025-07-21 22:24:32 +0300

    issue-1 is implementet

---
+++ Digiter/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Digiter;
+
+class Program
+{
+    static void Main(string[] args)
+    {      
+        {  
+            Console.WriteLine("Выбрать операцию: '+', '-', '/', '*'");
+
+            var str = Console.ReadLine();
+
+            Console.WriteLine("Введите первое число");
+
+            var numfirst = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число");
+
+            var numsecond = double.Parse(Console.ReadLine());
+
+            double result = 0;
+
+            switch (str)
+            {
+                case "+":
+                    result = (numfirst + numsecond);
+                    break;
+                case "-":
+                    result = (numfirst - numsecond);
+                    break;
+                case "/":
+                    result = (numfirst / numsecond);
+                    break;
+                case "*":
+                    result = (numfirst * numsecond);
+                    break;
+                default:
+                    Console.WriteLine("Ошибка");
+                    break;
+            }
+            Console.WriteLine($"Результат: {result}");            
+        }
+    }    
+}

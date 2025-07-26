e57e02024a9b4427cc7c378ccd1dd7df3ca85a51
Your Name <you@example.com>
2025-07-26 12:39:44 +0300

    task 2 is over

--- Digiter/Program.cs
+++ Digiter/Program.cs
@@ -3,41 +3,56 @@
 class Program
 {
     static void Main(string[] args)
-    {      
-        {  
-            Console.WriteLine("Выбрать операцию: '+', '-', '/', '*'");
+    {
+        {
+            bool abc = true;
 
-            var str = Console.ReadLine();
-
-            Console.WriteLine("Введите первое число");
-
-            var numfirst = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("Введите второе число");
-
-            var numsecond = double.Parse(Console.ReadLine());
-
-            double result = 0;
-
-            switch (str)
+            while (true)
             {
-                case "+":
-                    result = (numfirst + numsecond);
-                    break;
-                case "-":
-                    result = (numfirst - numsecond);
-                    break;
-                case "/":
-                    result = (numfirst / numsecond);
-                    break;
-                case "*":
-                    result = (numfirst * numsecond);
-                    break;
-                default:
-                    Console.WriteLine("Ошибка");
-                    break;
+                Console.WriteLine("Выбрать операцию: '+', '-', '/', '*'");
+
+                var str = Console.ReadLine();
+
+                Console.WriteLine("Введите первое число");
+
+                var numfirst = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите второе число");
+
+                var numsecond = double.Parse(Console.ReadLine());
+
+                double result = 0;
+
+                switch (str)
+                {
+                    case "+":
+                        result = (numfirst + numsecond);
+                        break;
+                    case "-":
+                        result = (numfirst - numsecond);
+                        break;
+                    case "/":
+                        result = (numfirst / numsecond);
+                        break;
+                    case "*":
+                        result = (numfirst * numsecond);
+                        break;
+                    default:
+                        Console.WriteLine("Ошибка");
+                        break;
+                }
+                Console.WriteLine($"Результат: {result}");
+
+                Console.WriteLine("Продолжить? (yes/no)");
+
+                string abcd = Console.ReadLine().ToLower();
+
+                abc = (abcd == "yes");
             }
-            Console.WriteLine($"Результат: {result}");            
         }
-    }    
+    }
 }
+
+
+
+
